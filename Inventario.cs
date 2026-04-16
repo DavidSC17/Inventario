2847058eace02cc953d25d07a08df92389dca832
David <javierinsan234@gmail.com>
2026-04-16 07:59:11 -0600

    Update Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,1 +1,24 @@
-
+Using System;
+public class inventario()
+{
+ public void ListaProductos()
+ {
+   
+ }
+  public void AgregarProducto(string nombre, int stock)
+  {
+    
+  }
+  public void BuscarProducto(string nombre)
+  {
+    
+  }
+  public void ActualizarStock(int id, int nuevoStock)
+  {
+    
+  }
+  public void EliminarProducto(int id)
+  {
+    
+  }
+}
